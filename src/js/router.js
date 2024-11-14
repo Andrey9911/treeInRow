7a268224d796952d9a9fef93968b0c00f75b9d0e
Andrey9911 <chivirevandrey775@gmail.com>
2024-11-14 15:00:39 +0300

    build #3 with changes becous not right build

--- src/js/router.js
+++ src/js/router.js
@@ -4,7 +4,7 @@ const routes = [
   { path: '/', component:() => import('../components/game.vue')},
   { path: '/history',component:() => import('../components/history.vue')},
   { path: '/profile',component:() => import('../components/profile.vue')},
-  { path: '/draw',component:() => import('../components/game.field.vue')},
+  { path: '/draw',component:() => import('../components/game_field.vue')},
 ]
 
 const router = createRouter({
